9aa7153aad2556670b72f40f3c8c38feb583c8e3
SamCalliskan <erdincscaliskan@gmail.com>
2025-11-15 18:55:42 +0300

    Update scratch_1.py

--- scratch_1.py
+++ scratch_1.py
@@ -1,5 +1,4 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+
 >>> 
 ... import time
 ... import random
@@ -59,6 +58,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
